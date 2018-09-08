b15b552cc57c815d066ac68f8f9bbda528e65761
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2018-09-07 23:24:44 -0500

    Base de datos implementada
    Tablas creadas y uniones implementadas dentro del ambiente sqlite

--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,5 +1,5 @@
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 const Sequelize = require('sequelize')
 const config = require('../config/config')
 const db = {}
@@ -11,15 +11,15 @@ const sequelize = new Sequelize(
   config.db.options
 )
 
-fs
-  .readdirSync(__dirname)
-  .filter((file) =>
-    file !== 'index.js'
-  )
-  .forEach((file) => {
-    const model = sequelize.import(path.join(__dirname, file))
-    db[model.name] = model
-  })
+// fs
+//   .readdirSync(__dirname)
+//   .filter((file) =>
+//     file !== 'index.js'
+//   )
+//   .forEach((file) => {
+//     const model = sequelize.import(path.join(__dirname, file))
+//     db[model.name] = model
+//   })
 
 db.sequelize = sequelize
 db.Sequelize = Sequelize
