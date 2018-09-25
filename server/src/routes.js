903252a2dc2f03b36be4b83ac8542839685c3836
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2018-09-24 20:48:06 -0500

    Add files via upload

--- server/src/routes.js
+++ server/src/routes.js
@@ -1,7 +1,13 @@
 const AuthenticationController = require('./controllers/AuthenticationControllers')
 const AuthenticationControllerPolicy = require('./policies/AuthenticationControllerPolicy')
+const LendController = require('./controllers/LendControllers')
+const EquipRegisterController = require('./controllers/EquipRegisterControllers')
 
 module.exports = (app) => {
   app.post('/login', AuthenticationController.login)
   app.post('/register', AuthenticationControllerPolicy.register, AuthenticationController.register)
+
+  app.get('/lend', LendController.index)
+
+  app.post('/equip', EquipRegisterController.register)
 }
