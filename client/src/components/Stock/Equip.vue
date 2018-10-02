a9de40e21f514f8c487290e264bccf5db19b7bdc
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2018-10-01 20:45:07 -0500

    Add files via upload

---
+++ client/src/components/Stock/Equip.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <panel title="Equipos">
+    <v-card>
+      <v-card-title>
+        <v-text-field
+          v-model="search"
+          label="Buscar"
+          single-line
+          hide-details
+        ></v-text-field>
+        <v-spacer></v-spacer>
+        <v-spacer></v-spacer>
+      </v-card-title>
+
+        <v-data-table
+          :headers="headers"
+          :items="items"
+          :search="search">
+
+          <template slot="headers" slot-scope="props">
+            <tr>
+              <th
+                v-for="header in props.headers"
+                :key="header.text">
+                <strong> {{ header.text }} </strong>
+              </th>
+            </tr>
+          </template>
+
+          <template slot="items" slot-scope="props">
+            <tr>
+              <td>{{ props.item.CodBarras }}</td>
+              <td>{{ props.item.Nombre }}</td>
+            </tr>
+          </template>
+
+          <v-alert slot="no-results" :value="true" color="error">
+            La busqueda de "{{ search }}" no entregó resultados.
+          </v-alert>
+
+        </v-data-table>
+      </v-card>
+    </panel>
+  </div>
+</template>
+<script>
+import EquipmentService from '@/services/EquipmentService'
+import Panel from '@/components/Panel'
+
+export default {
+  data () {
+    return {
+      items: [],
+      search: '',
+      headers: [
+        { text: 'Código de Barras', value: 'CodBarras' },
+        { text: 'Nombre', value: 'Nombre' }
+      ]
+    }
+  },
+  async mounted () {
+    const data = (await EquipmentService.indexEquip()).data
+    for (let i = 0; i < data.equipInfo.length; i++) {
+      this.items.push(data.equipInfo[i])
+    }
+  },
+  components: {
+    Panel
+  }
+}
+</script>
+<style scoped>
+</style>
