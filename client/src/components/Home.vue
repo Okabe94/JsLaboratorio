6c466cdc1235a33855a596146c1c81d51f3d904e
Okabe94 <33587455+Okabe94@users.noreply.github.com>
2018-10-20 07:50:26 -0500

    ¡Pruebas necesarias!
    Se arregla la muestra de información en la tabla principal del programa

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,85 +1,85 @@
 <template>
   <div>
     <panel title="Préstamos Activos">
-    <v-card>
-      <v-card-title>
-        <v-text-field
-          v-model="search"
-          label="Buscar"
-          single-line
-          hide-details
-        ></v-text-field>
-        <v-spacer></v-spacer>
-        <v-spacer></v-spacer>
-        <v-btn
-        v-if="$store.state.isLoggedIn"
-        @click="navigateTo({ name: 'lend' })"
-        dark
-        class="green darken-5 font-weight-bold">
-          Nuevo Préstamo
-        </v-btn>
-      </v-card-title>
+      <v-card>
+        <v-card-title>
+          <v-text-field
+            v-model="search"
+            label="Buscar"
+            single-line
+            hide-details
+          ></v-text-field>
+          <v-spacer></v-spacer>
+          <v-spacer></v-spacer>
+          <v-btn
+          v-if="$store.state.isLoggedIn"
+          @click="navigateTo({ name: 'lend' })"
+          dark
+          class="green darken-5 font-weight-bold">
+            Nuevo Préstamo
+          </v-btn>
+        </v-card-title>
 
         <v-data-table
           :headers="headers"
           :items="items"
           :search="search"
-          item-key="Carnet">
+          item-key="_id">
 
-        <template slot="headers" slot-scope="props">
-          <tr>
-            <th
-              v-for="header in props.headers"
-              :key="header.text">
-              <strong> {{ header.text }} </strong>
-            </th>
-          </tr>
-        </template>
+          <template slot="headers" slot-scope="props">
+            <tr>
+              <th
+                v-for="header in props.headers"
+                :key="header.text">
+                <strong> {{ header.text }} </strong>
+              </th>
+            </tr>
+          </template>
 
-        <template slot="items" slot-scope="props">
-          <tr @click="details (props, props.item.Carnet)">
-            <td>{{ props.item.items }}</td>
-            <td>{{ props.item.estudiante }}</td>
-            <td>{{ props.item.carnet }}</td>
-            <td>{{ props.item.salon }}</td>
-            <td>{{ props.item.modulo }}</td>
-            <td>{{ props.item.monitor }}</td>
-          </tr>
-        </template>
+          <template slot="items" slot-scope="props">
+            <tr @click="details(props, props.item._id)">
+              <td>{{ props.item.estudiante.nombre }}</td>
+              <td>{{ props.item.estudiante.carnet }}</td>
+              <td>{{ props.item.modulo.salon }}</td>
+              <td>{{ props.item.modulo.numero }}</td>
+              <td>{{ props.item.monitor.nombre }}</td>
+            </tr>
+          </template>
 
-        <template slot="expand" slot-scope="props">
-          <v-data-table
+          <template slot="expand" slot-scope="props">
+            <v-data-table
             hide-actions
             :headers="detailHeaders"
             :items="detailItems">
 
-            <template slot="headers" slot-scope="props">
-              <tr>
-                <th
-                  v-for="header in props.headers"
-                  :key="header.text">
-                  <strong> {{ header.text }} </strong>
-                </th>
-              </tr>
-            </template>
+              <template slot="headers" slot-scope="props">
+                <tr>
+                  <th
+                    v-for="header in props.headers"
+                    :key="header.text">
+                    <strong> {{ header.text }} </strong>
+                  </th>
+                </tr>
+              </template>
 
-            <template slot="items" slot-scope="props">
-              <tr>
-                <td>{{ props.item.Equipo }}</td>
-                <td>{{ props.item.Cantidad }}</td>
-              </tr>
-            </template>
+              <template slot="items" slot-scope="props">
+                <tr>
+                  <td>{{ props.item.nombre }}</td>
+                  <td>{{ props.item.codBarras }}</td>
+                  <td>{{ props.item.cantidad }}</td>
+                </tr>
+              </template>
 
-          </v-data-table>
-        </template>
+            </v-data-table>
+          </template>
 
-        <v-alert slot="no-results" :value="true" color="error">
-          La busqueda de "{{ search }}" no entregó resultados.
-        </v-alert>
+          <v-alert slot="no-results" :value="true" color="error">
+            La busqueda "{{ search }}" no entregó resultados.
+          </v-alert>
 
-        <template slot="pageText" slot-scope="props">
-          Lignes {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
-        </template>
+          <template slot="pageText" slot-scope="props">
+            Lignes {{ props.pageStart }} - {{ props.pageStop }} de {{ props.itemsLength }}
+          </template>
 
         </v-data-table>
       </v-card>
@@ -89,14 +89,13 @@
 
 <script>
 import Panel from '@/components/Panel'
-import HomeTableService from '@/services/HomeTableService'
+import HomeService from '@/services/HomeService'
 
 export default {
   data () {
     return {
       search: '',
       headers: [
-        { text: 'Préstamos', value: 'items', align: 'left' },
         { text: 'Estudiante', value: 'estudiante' },
         { text: 'Carnet', value: 'carnet' },
         { text: 'Salon', value: 'salon' },
@@ -105,18 +104,17 @@ export default {
       ],
       detailHeaders: [
         { text: 'Equipo', value: 'equipo' },
+        { text: 'Código de barras', value: 'codBarras' },
         { text: 'Cantidad', value: 'cantidad' }
       ],
-      items: [],
-      detailItems: []
+      items: []
     }
   },
   components: {
     Panel
   },
   async mounted () {
-    const data = (await HomeTableService.indexHome()).data
-    console.log(data)
+    const data = (await HomeService.indexHome()).data
     for (let i = 0; i < data.index.length; i++) {
       this.items.push(data.index[i])
     }
@@ -125,15 +123,22 @@ export default {
     navigateTo (route) {
       this.$router.push(route)
     },
-    async details (props, carnet) {
+    details (props, id) {
       if (!props.expanded) {
-        const detail = (await HomeTableService.indexDetails({ Carnet: carnet })).data
         this.detailItems = []
-        for (let i = 0; i < detail.homeInfoDetail.length; i++) {
-          this.detailItems.push(detail.homeInfoDetail[i])
+        const detail = this.find(id)
+        for (let i = 0; i < detail.length; i++) {
+          this.detailItems.push(detail[i])
         }
       }
       props.expanded = !props.expanded
+    },
+    find (id) {
+      for (let i = 0; i < this.items.length; i++) {
+        if (this.items[i]._id === id) {
+          return this.items[i].equipo
+        }
+      } return []
     }
   }
 }
